77e12923abde7aa7f3da5dcb354edcb44f56e008
ThomasTacoVos <tvvos@hotmail.nl>
2018-05-15 11:20:22 +0200

    Thomas test v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Taco
+                    Laravel Taco Thomas v2
                 </div>
 
                 <div class="links">
