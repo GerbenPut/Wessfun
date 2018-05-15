d15a7a42ea19546a03e80e408de645e425bfd13b
Joris <joris.wessels@hotmail.com>
2018-05-15 11:08:40 +0200

    joris test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel joris
                 </div>
 
                 <div class="links">
