65c8253b0e71e486decd533fe13a1d468b494d85
Joris <joris.wessels@hotmail.com>
2018-05-16 13:29:13 +0200

    ghello joris

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel joris
+                    Laravel joris ghello
                 </div>
 
                 <div class="links">
