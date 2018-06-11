a32809ce6caae23c64be7f7a66122c1682b125cc
JorisWessels <joris.wessels@hotmail.com>
2018-06-11 22:43:17 +0200

    Relations.

--- resources/views/Images/show.blade.php
+++ resources/views/Images/show.blade.php
@@ -1,10 +1,10 @@
 
-    <div class="blog-post">
-        <h2 class="blog-post-title">{{$image->title}}</h2>
-        <p>{{$image->description}}</p>
-        <img src="{{$image->url}}" style="height: 50%; width: auto">
-    </div>
-    <a href="http://127.0.0.1:8000/">Back to home</a>
+    {{--<div class="blog-post">--}}
+        {{--<h2 class="blog-post-title">{{$image->title}}</h2>--}}
+        {{--<p>{{$image->description}}</p>--}}
+        {{--<img src="{{$image->url}}" style="height: 50%; width: auto">--}}
+    {{--</div>--}}
+    {{--<a href="http://127.0.0.1:8000/">Back to home</a>--}}
 
 @foreach ($images as $image)
     @foreach ($image->categories as $category)
